29518c46fbdbc04a42d5e56efa8b602a97732e19
Saw S. Lin <sawsanlin.zhiqi@gmail.com>
2023-10-02 21:37:03 +0630

    Finished exercises 10.11.

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,15 +1,17 @@
 import Main from './src/components/Main';
 import { NativeRouter } from 'react-router-native';
-import { StatusBar } from 'expo-status-bar';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
 	return (
-		<>
-			<NativeRouter>
+		<NativeRouter>
+			<ApolloProvider client={apolloClient}>
 				<Main />
-			</NativeRouter>
-			<StatusBar style='auto' />
-		</>
+			</ApolloProvider>
+		</NativeRouter>
 	);
 };
 
---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,34 @@
+// import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../components/graphql/queries';
+
+const useRepositories = () => {
+  // const [repositories, setRepositories] = useState();
+  // const [loading, setLoading] = useState(false);
+
+	const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: 'cache-and-network',
+	});
+
+  // const fetchRepositories = async () => {
+  //   setLoading(true);
+
+  //   // Replace the IP address part with your own IP address!
+  //   const response = await fetch('http://192.168.100.110:5001/api/repositories');
+  //   const json = await response.json();
+
+  //   setLoading(false);
+  //   setRepositories(json);
+  // };
+
+  // useEffect(() => {
+  //   fetchRepositories();
+  // }, []);
+
+  // return { repositories, loading, refetch: fetchRepositories };
+
+	return { repositories: data ? data.repositories: null, loading, refetch };
+};
+
+export default useRepositories;
